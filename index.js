f1dee99f4667514733d9a04e19cd2055f90fe955
Jesús Adolfo Parra Parra <jesus.parra@opticca.com>
2019-01-17 22:09:29 -0500

    cloning and checking out

--- index.js
+++ index.js
@@ -7,17 +7,39 @@ exports.buildlauncher = async (req, res) => {
   });
   const projectId = await google.auth.getProjectId();
 
-  const requestBody = {
-    steps: [{
-        name: "gcr.io/cloud-builders/gsutil",
-        entrypoint: "bash",
-        args: ["-c", "echo 'simple build from cloud function'"]
-      }]
-  };
+  const event = req.get('X-GitHub-Event');
+  const repository = req.body.repository;
 
-  const newOperation = await cloudbuild.projects.builds.create({
-    auth, projectId, requestBody });
+  if (event == "push") {
+    console.log("Push event");
 
-  console.log(newOperation.data.name);
+    const requestBody = {
+      steps: [
+        {
+          name: "gcr.io/cloud-builders/gsutil",
+          entrypoint: "bash",
+          args: ["-c", "echo 'simple build from cloud function'"]
+        },
+        {
+          name: "gcr.io/cloud-builders/git",
+          args: ["clone", `${repository.clone_url}`, '.']
+        },
+        {
+          name: "gcr.io/cloud-builders/git",
+          args: ["checkout", `${req.body.head_commit.id}`]
+        },
+        {
+          name: "gcr.io/cloud-builders/git",
+          args: ["log", "--oneline"]
+        }
+      ]
+    };
+
+    const newOperation = await cloudbuild.projects.builds.create({
+      auth, projectId, requestBody });
+
+    console.log(newOperation.data.name);
+  }
+
   res.status(200).send('Hello world');
 }
